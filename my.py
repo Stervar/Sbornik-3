c95a4e8319a6306ad5eaf9d20991311e195cdddd
stervar <gaberkonvadim8@gmail.com>
2024-08-28 19:38:38 +0300

    C:\Users\279014673\AppData\Roaming\Python\Python312\Scripts

--- my.py
+++ my.py
@@ -5895,3 +5895,57 @@
 ## Требуется дополнительное время и усилия для изучения и применения паттернов
 ## Необходимость тщательного планирования и проектирования системы
 ## В целом, паттерны проектирования - это мощный инструмент для разработчиков, который помогает создавать болееflexible, maintainable и scalable системы. Однако, их применение требует тщательного планирования и проектирования, а также дополнительного времени и усилий для изучения и применения.
+
+
+
+
+
+## Задание 1
+## Создайте реализацию паттерна Builder. Протестируйте
+## работу созданного класса.
+
+
+
+class Car:
+    def __init__(self):
+        self.make = None
+        self.model = None
+        self.year = None
+        self.color = None
+
+    def __str__(self):
+        return f"Car: {self.year} {self.color} {self.make} {self.model}"
+
+class CarBuilder:
+    def __init__(self):
+        self.car = Car()
+
+    def set_make(self, make):
+        self.car.make = make
+        return self
+
+    def set_model(self, model):
+        self.car.model = model
+        return self
+
+    def set_year(self, year):
+        self.car.year = year
+        return self
+
+    def set_color(self, color):
+        self.car.color = color
+        return self
+
+    def build(self):
+        return self.car
+
+# Testing the CarBuilder class
+builder = CarBuilder()
+car = (builder
+       .set_make("Toyota")
+       .set_model("Camry")
+       .set_year(2022)
+       .set_color("Silver")
+       .build())
+
+print(car)  # Output: Car: 2022 Silver Toyota Camry
