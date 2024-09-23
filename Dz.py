d078b152234a0142dbff3d82c26500d499f16a23
Stervar <150718158+Stervar@users.noreply.github.com>
2024-09-24 02:09:27 +0300

    vg

--- Dz.py
+++ Dz.py
@@ -87,9 +87,14 @@
 
 
 
-# №5
-
-
+# Задание 5
+# Выведите на экран надпись «Life is what happens when
+# you're busy making other plans» John Lennon на разных
+# строках. Пример вывода:
+# “Life is what happens
+# when
+# you’re busy making other plans”
+# John Lennon.
 
 
 
